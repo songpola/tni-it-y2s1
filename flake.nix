fba92b874c262424d3b5bcda0d13018ab41a1cab
Songpol Anannetikul <1527535+songpola@users.noreply.github.com>
2025-07-13 10:35:48 +0700

    Use flake instead of devbox

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    devshell.url = "github:numtide/devshell";
+    devshell.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devshell.flakeModule
+      ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          devshells.default = {
+            packages = with pkgs; [
+              typst
+            ];
+            env = [
+              {
+                name = "TYPST_FONT_PATHS";
+                value = "./font";
+              }
+            ];
+          };
+        };
+    };
+}
